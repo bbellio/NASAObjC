d0b2f59a4b1035d97c9723ec5b4f3b18ad34028e
Bethany Wride <bethanywride@Bethanys-MacBook-Air-2.local>
2019-10-09 22:27:13 -0600

    Created initial project file and organized folders

---
+++ NASAObj-C/NASAObj-C/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NASAObj-C
+//
+//  Created by Bethany Wride on 10/9/19.
+//  Copyright © 2019 Bethany Wride. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ NASAObj-C/NASAObj-C/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  NASAObj-C
+//
+//  Created by Bethany Wride on 10/9/19.
+//  Copyright © 2019 Bethany Wride. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
